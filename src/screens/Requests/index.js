7aa11c89b80eb0ae901229777fc2cbbbafeddadb
Aun Raza <aunraza03222@gmail.com>
2022-01-29 19:48:41 +0500

    using array for tabs

--- src/screens/Requests/index.js
+++ src/screens/Requests/index.js
@@ -16,53 +16,65 @@ const Requests = () => {
       setAccepted(false);
     }
   };
+  const tabArray = [I18n.t('accepted_text'), I18n.t('pending_text')];
   return (
     <View style={styles.main_view}>
       <View style={styles.tabs_view}>
-        <TouchableOpacity
-          onPress={() => getRequest('accepted')}
-          style={[
-            styles.pending_view,
-            {
-              backgroundColor: accepted
-                ? COLORS.green_color
-                : COLORS.white_color,
-              borderTopLeftRadius: SIZES.padding,
-              borderBottomLeftRadius: SIZES.padding,
-            },
-          ]}>
-          <Text
-            style={[
-              styles.text,
-              {
-                color: accepted ? COLORS.white_color : COLORS.black_color,
-              },
-            ]}
-            text={I18n.t('accepted_text')}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => getRequest('pending')}
-          style={[
-            styles.pending_view,
-            {
-              backgroundColor: pending
-                ? COLORS.maroon_color
-                : COLORS.white_color,
-              borderTopRightRadius: SIZES.padding,
-              borderBottomRightRadius: SIZES.padding,
-            },
-          ]}>
-          <Text
-            style={[
-              styles.text,
-              {
-                color: pending ? COLORS.white_color : COLORS.black_color,
-              },
-            ]}
-            text={I18n.t('pending_text')}
-          />
-        </TouchableOpacity>
+        {tabArray?.map((v, i) => {
+          return (
+            <TouchableOpacity
+              key={i}
+              onPress={() =>
+                i === 0 ? getRequest('accepted') : getRequest('pending')
+              }
+              style={
+                i === 0
+                  ? [
+                      styles.pending_view,
+                      {
+                        backgroundColor: accepted
+                          ? COLORS.green_color
+                          : COLORS.white_color,
+                        borderTopLeftRadius: SIZES.padding,
+                        borderBottomLeftRadius: SIZES.padding,
+                      },
+                    ]
+                  : [
+                      styles.pending_view,
+                      {
+                        backgroundColor: pending
+                          ? COLORS.maroon_color
+                          : COLORS.white_color,
+                        borderTopRightRadius: SIZES.padding,
+                        borderBottomRightRadius: SIZES.padding,
+                      },
+                    ]
+              }>
+              <Text
+                style={
+                  i === 0
+                    ? [
+                        styles.text,
+                        {
+                          color: accepted
+                            ? COLORS.white_color
+                            : COLORS.black_color,
+                        },
+                      ]
+                    : [
+                        styles.text,
+                        {
+                          color: pending
+                            ? COLORS.white_color
+                            : COLORS.black_color,
+                        },
+                      ]
+                }
+                text={v}
+              />
+            </TouchableOpacity>
+          );
+        })}
       </View>
     </View>
   );
